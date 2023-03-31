ef9e1f5834ed75d40f3b8fd66a9f5167ece51528
Jorge Caballero <joalbes@gmail.com>
2023-03-31 10:59:31 -0500

    31 marzo 10:59am

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+//varibalbes
+let alamcenamientoData = [];
 //vistas
 const formulario = document.getElementById('formulario');
 const contenedorTabla = document.getElementById('contenedorTabla');
 //evento
 formulario.addEventListener('submit',(e)=>{
+    //elimina el comportamineto por defecto del evento submit
     e.preventDefault();
+    // creo un objeto con todos los datos del formulario
     const objDatos = new FormData(formulario);
+    //creo objeto que almacena en variables el valor de los inputs del formulario.
     let objVariableDatos = crearData(objDatos);
+    //guardo en el local storage los inputs del formulario
+    guardarObjlLocalStorage(objVariableDatos);
+    //lleno la tabla: creando una fila en la ultima posicion e ingreso las 4 celdas.
     llenarTabla(objVariableDatos);
     // contenedorTabla.innerHTML+=`
     // <tr class="border-solid">
@@ -28,12 +36,19 @@ function crearData(obj) {
     
 }
 
+function guardarObjlLocalStorage(obj) {
+    //almaceno en vector cada obj nuevo.
+    alamcenamientoData.push(obj);
+    //guardo en el localStorage.
+    localStorage.setItem('objVariablesData',JSON.stringify(alamcenamientoData));
+}
+
 function llenarTabla(obj) {
     // agrego Fila
     let fila = contenedorTabla.insertRow(-1);
     //agrego celda0
     let celda = fila.insertCell(0);
-    celda.textContent = obj.transactionType
+    celda.textContent = obj['transactionType']
     // agrego celda1
     celda = fila.insertCell(1);
     celda.textContent = obj.transactionDescription
